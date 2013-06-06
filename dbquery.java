a451bf914b369632a4d07ebb7c950a2a32dd0948
Mauro Masciadro <mauro.masciar@hotmail.com>
2013-06-05 21:03:51 -0300

    Add more examples

---
+++ dbquery.java
@@ -0,0 +1,53 @@
+package CodigosAImplementar;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+class dbquery {
+
+    private final static String drv = "com.mysql.jdbc.Driver";
+    private final static String db = "jdbc:mysql://localhost:1122";
+    private final static String user = "user";
+    private final static String pass = "password";
+    private Connection ct;
+    private Statement st;
+
+    public void connect() {
+        try {
+            Class.forName(drv);
+            ct = DriverManager.getConnection(db, user, pass);
+            st = ct.createStatement();
+            System.out.println("Connected");
+
+            st.close();
+            ct.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void insertData() {
+        try {
+            Class.forName(drv);
+            ct = DriverManager.getConnection(db, user, pass);
+            st = ct.createStatement();
+
+            String insert = "INSERT INTO `Account` (`Name`) VALUES ( 'Mauro');";
+            //String insertTextField = "INSERT INTO `Account` (`Name`) VALUES ('" + txtName.getText() + "')";
+            String delete = "DELETE FROM `Account` WHERE id = 1;";
+            String createTable = "CREATE TABLE `Account` ( `Name` varchar(45) DEFAULT NULL";
+            
+            st.executeUpdate(insert); //Example: execute insert
+
+            st.close();
+            ct.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        dbquery app = new dbquery();
+    }
+}
---
+++ screenshot.java
@@ -0,0 +1,26 @@
+/* Take a screenshot */
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class screenshot {
+	public static void main(String[] args) throws IOException {
+		try
+			{
+				Robot robot = new Robot();
+				BufferedImage screen = robot.createScreenCapture(new Rectangle (Toolkit.getDefaultToolkit().getScreenSize()));
+				File file = new File("C:\\screen.jpg");
+				ImageIO.write(screen, "jpg", file);
+			}
+		catch (AWTException e)
+			{
+				e.printStackTrace();
+			}
+		}
+}
